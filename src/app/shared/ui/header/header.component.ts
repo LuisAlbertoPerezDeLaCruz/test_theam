9cecfea495e4c8cec74973305a9c586f99cedb9e
Luis Alberto Perez <luisalbertoperez@gmail.com>
2024-09-21 19:29:21 -0400

    avance

--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface MenuOption {
+  route: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styles: ``
+  styles: ``,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userMenuOptions: MenuOption[] = [];
+  administratorMenuOptions: MenuOption[] = [];
+  mainMenu: MenuOption[] = [];
+  ngOnInit() {
+    this.mainMenu = this.loadOptions('user');
+  }
+  loadOptions(userType: string): MenuOption[] {
+    if (userType === 'user') {
+      return [
+        ...this.userMenuOptions,
+        ...[
+          {
+            route: '/',
+            text: 'Home',
+          },
+          {
+            route: '/planes',
+            text: 'Planes',
+          },
+          {
+            route: '/recargas',
+            text: 'Recargar',
+          },
+          {
+            route: '/ayuda',
+            text: 'Ayuda',
+          },
+        ],
+      ];
+    }
 
+    if (userType === 'admin') {
+      return [];
+    }
+    return [];
+  }
 }
